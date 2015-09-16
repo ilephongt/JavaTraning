3fada3ce99e0084e7cf5b9845267eebaaed15de6
ilephongt <ilephongt@gmail.com>
2015-09-16 17:13:10 +0700

    Create QA.java

---
+++ QA.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class QA{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("xin chao ban ten gi");
+		String a = scanner.nextLine();
+		System.out.println("Chao " + a);
+		System.out.println("Ban dang o dau vay?");
+		String b = scanner.nextLine();
+		System.out.println("ah, dia diem "+b+ " rat dep! Chuc mot ngay tot lanh");
+
+
+	}
+
+
+
+}
