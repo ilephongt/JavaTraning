b84e9e8caca22b7fa021e676b6d1b5c7e2b6458d
ilephongt <ilephongt@gmail.com>
2015-09-21 17:25:38 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,36 +1,32 @@
 import java.util.Scanner;
 public class PrimeNumber{
 	public static boolean checkPrimeNumber(int a){
-		if ((a == 1)||(a == 0)){
-			return false;
-		}	
-		else{
+		
+ 
+    if((a==0)||(a==1)) return false;
+    for(int i=2;i<a;i++) {
+        if(a%i==0)
+            return false;
+    }
+    return true;
+}
+	
 
-				for (int i = 2; i<a; i++){
-			//	if(a==2) continue;
-				if(a%i == 0){
-				return false;
-
-				}
-
-		}
-	}
-
-		return true;
-
-	}
+	
 
 
 public static void main(String[] args) {
 	
 	Scanner scanner = new Scanner(System.in);
+	PrimeNumber check = new PrimeNumber();
 	System.out.println("nhap so nguyen a:");
 	int a = scanner.nextInt();
-	PrimeNumber check = new PrimeNumber();
-	if (check.checkPrimeNumber(a) == true){
+	
+	if (check.checkPrimeNumber(a)&&(a>0)){
 
 		System.out.println(a+" la so nguyen to!");
 	}
 	else System.out.println(a+" khong phai la so nguyen to!");
+
 }
 }
