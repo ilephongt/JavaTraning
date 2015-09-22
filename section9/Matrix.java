2b4d15a2573692e230a57018cbe034dfc1cf6016
ilephongt <ilephongt@gmail.com>
2015-09-22 10:01:40 +0700

    Update Matrix.java

--- section9/Matrix.java
+++ section9/Matrix.java
@@ -4,7 +4,7 @@ class Matrix {
     public static void main( String[] args ) { 
 
         List<List<Integer>> array = new ArrayList<List<Integer>>();
-         List<List<Integer>> array2 = new ArrayList<List<Integer>>();
+        List<List<Integer>> array2 = new ArrayList<List<Integer>>();
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter cot:");
@@ -19,14 +19,14 @@ class Matrix {
             // add row:
             List<Integer> list = new ArrayList<Integer>();
             array.add(list);//dua cac bien trong mang 1 chieu sang mang 2 chieu
+            System.out.println("Nhap hang thu "+(i+1));
             for( int j = 0 ; j < n ; j++) {                
                 list.add( input.nextInt());
             }
         }
 
-        System.out.println("mang 2 chieu vua nhap");
+        System.out.println("ma tran vua nhap");
         System.out.println( array );
-
         int k;
         k=m*n;
         int[] motchieu = new int[k];
@@ -40,27 +40,33 @@ class Matrix {
         }     
 
 //sap xep mang motchieu
-         int temp = 0;
+        int temp = 0;
+        for (int i = 0; i < motchieu.length; i++) {
+            for (int j = 1; j < (motchieu.length - i); j++) {
 
-    for (int i = 0; i < motchieu.length; i++) {
-        for (int j = 1; j < (motchieu.length - i); j++) {
-
-            if (motchieu[j - 1] > motchieu[j]) {
+                if (motchieu[j - 1] > motchieu[j]) {
                 temp = motchieu[j - 1];
                 motchieu[j - 1] = motchieu[j];
                 motchieu[j] = temp;
+                }
+
             }
-
         }
-    }
 //chuyen du lieu tu motchieu vao list roi vao vao array2
-    for(int i=0;i<motchieu.length;i++){
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(motchieu[i]);
-        array2.add(list);
+        int p=0;
+        for(int i=0;i<m;i++){
+        List<Integer> list2 = new ArrayList<Integer>();
+       
+            for(int j=0;j<n;j++){
+
+            list2.add(motchieu[p]);
+            ++p;
+            }
+        array2.add(list2);
+        }
+    
+         System.out.println("sau khi sap xep: \n"+array2);
+
     }
 
-         System.out.println(array2);
-
     }
-}
