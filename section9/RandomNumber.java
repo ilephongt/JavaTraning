021bd13fbb62d8f336fbeaf5075087b6938e5ea3
ilephongt <ilephongt@gmail.com>
2015-09-22 12:14:13 +0700

    Update RandomNumber.java

--- section9/RandomNumber.java
+++ section9/RandomNumber.java
@@ -7,16 +7,22 @@ public class RandomNumber{
         List<Float> array3 = new ArrayList<Float>();
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
-        float tam1;
+        float tam;
+        float tam2 = 0;
         array3.addAll(array);
-
+        
 
 //sinh so random                
         System.out.println("A.ban muon random bao nhieu so?");
-        float n = sc.nextFloat();
-        for(int i =0; i<n;i++){
-            tam1=rd.nextFloat();
-            array.add(tam1);
+        int kiemtra=sc.nextInt();
+
+        while(kiemtra<=0){
+            System.out.println("nhap lai, chi nhap so nguyen >0");
+            kiemtra=sc.nextInt();
+        }        
+        for(int i =0; i<kiemtra;i++){
+            tam=rd.nextFloat();
+            array.add(tam);
         }
         System.out.println("\n\ndanh sach cac so duoc tao:\n"+array);
         array3.addAll(array);//copy array nay vao array3 de dung` o cau D 
@@ -34,35 +40,45 @@ public class RandomNumber{
         
 
 //in ra thu tu giam dan
-        Collections.sort(array);         
-        int a=array.size();
+        int dem =0;
+        int a = array.size();
         float[] motchieu= new float[a];
-
-
-//truyen cac phan tu vao mang 1 chieu
         for(Float num : array){
-        	motchieu[a-1]=num;
-       		a--;
+            motchieu[dem] = num;
+            ++dem;
+        }//chuyen du lieu sang motchieu
+
+        for (int i = 0; i < motchieu.length; i++) {
+            for (int j = 1; j < (motchieu.length - i); j++) {
+
+                if (motchieu[j - 1] < motchieu[j]) {
+                tam2 = motchieu[j - 1];
+                motchieu[j - 1] = motchieu[j];
+                motchieu[j] = tam2;
+                }
+
+            }
         }
+       
+        ///sap xep motchieu giam dan
 
 
-//chuyen motchieu vao array2
+        System.out.println("\n\nC.thu tu giam dan cua day la:\n");
         for (int i=0;i<motchieu.length;i++){
-            array2.add(motchieu[i]);    
-        }         
-        System.out.println("\n\nC.thu tu giam dan cua day la:\n"+array2);
+            System.out.print(motchieu[i]+", ");
+        }
 
           
  //nhap vao gia tri x va xoa cac so x co trong array
+
         System.out.println("\nD.nhap vao 1 so bat ky de xoa");
         float b = sc.nextFloat();
-        List<Float> toRemove = new ArrayList<Float>();
-            for (int i=0; i<array3.size();i++){
-                if(array3.get(i)==b){
-                    array3.remove(i);
+            for (int i=0; i<array.size();i++){
+                if(array.get(i)==b){
+                    array.remove(i);
                 }
             }    
-        System.out.println("\n\ndanh sach cac so sau khi xoa: "+array3);
+        System.out.println("\n\ndanh sach cac so sau khi xoa: "+array);
 
     }
 
