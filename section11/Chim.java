c186dac75bd1d9fd66ff3bd7a558b02da00c7ec4
lephong <ilephongt@gmail.com>
2015-09-23 16:49:33 +0700

    Create Chim.java

---
+++ section11/Chim.java
@@ -0,0 +1,20 @@
+public class Chim extends GiaCam{
+	public void di(){
+		super.di();
+		System.out.println("chim co the di");
+	}
+	public void bay(){
+		super.bay();
+		System.out.println("Chim co the bay");
+	}
+	public void boi(){
+		super.boi();
+		System.out.println("chim co the boi");
+	}
+
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la Chim");
+	}
+}
+}
