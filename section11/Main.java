fadc80001ffc7466536ef7ea8afceb21a3e406bd
lephong <ilephongt@gmail.com>
2015-09-23 16:55:48 +0700

    Create Main.java

---
+++ section11/Main.java
@@ -0,0 +1,12 @@
+
+class Main{
+
+   public static void main(String args[]){
+
+      Animal test = new ChoNhat();
+      test.gioiThieu();
+      test.di();
+      test.boi();
+      test.bay();
+    }
+}
