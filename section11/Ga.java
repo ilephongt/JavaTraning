f44360289a3b94cc8cf9b424e7a7a121bbfef22b
lephong <ilephongt@gmail.com>
2015-09-23 16:48:18 +0700

    Create Ga.java

---
+++ section11/Ga.java
@@ -0,0 +1,18 @@
+public class Ga extends GiaCam{
+	public void di(){
+		super.di();
+		System.out.println("ga co the di");
+	}
+	public void bay(){
+		super.bay();
+		System.out.println("ga co the bay");
+	}	
+	public void boi(){
+		super.boi();
+		System.out.println("ga khong the boi");
+	}
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la Ga");
+	}
+}
