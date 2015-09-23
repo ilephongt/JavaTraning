a5afc08c94bbd5bbc866b603157993239c7ea5e4
lephong <ilephongt@gmail.com>
2015-09-23 10:45:53 +0700

    Update CalculatorFinal.java

--- section10/CalculatorFinal.java
+++ section10/CalculatorFinal.java
@@ -9,8 +9,8 @@ import java.util.*;
 public class CalculatorFinal{
 	private float numberOne;
 	private float numberTwo;
-	private char operator;
-
+	private static char operator;
+	private float ketqua;
 	public boolean setOperator(char operator){
 		
 		if((operator=='+')||(operator=='-')||(operator=='*')||(operator=='/')){
@@ -37,34 +37,30 @@ public class CalculatorFinal{
 		return this.numberTwo;
 	}
 
-	public String result(){
-		
+	public void solution(){
+			
 			switch(operator){
 				case '+': 
-					System.out.println(""+(numberOne+numberTwo));
-				break;	
-					
-					
+					ketqua=numberOne+numberTwo;
+					break;
+
 				case '-': 
-					System.out.println(""+(numberOne-numberTwo));
-				break;
+					ketqua=numberOne-numberTwo;
+					break;
 					
 				case '*': 
-					System.out.println(""+(numberOne*numberTwo));
+					ketqua=numberOne*numberTwo;
+					break;
 
-				break;	
-				case '/': 	if(numberTwo==0) 
-							System.out.println("k chia dc");
-							else{
-						  	System.out.println(""+(numberOne/numberTwo));
-
-						    }
-				break;
-			
+				case '/':					
+					ketqua=numberOne/numberTwo;
+					break;
 			}
-			return "";
-			
-		} 
+		}
+
+	public float result(){
+		return this.ketqua;
+	}
 
 	public static void main(String[] args) {
 		CalculatorFinal cal = new CalculatorFinal();
@@ -79,15 +75,35 @@ public class CalculatorFinal{
 		cal.setNumberTwo(b);
 		Scanner sc2 = new Scanner(System.in);
 		boolean flag2=false;
-		while(flag2==false){
-			System.out.println("nhap phep tinh:");			
-			char operator = sc2.nextLine().charAt(0);
 
+		while(flag2==false){
+		System.out.println("nhap phep tinh:");			
+		char operator = sc2.nextLine().charAt(0);
+		cal.setOperator(operator);
 			if (cal.setOperator(operator)==true){
 				flag2 = true;
 			}
 		}
-		cal.result();
+		switch(operator){
+			case '+': cal.solution();
+					  System.out.println("ket qua = "+cal.result());
+					  break;
+			case '-': cal.solution();
+					  System.out.println("ket qua = "+cal.result());
+					  break;
+			case '*': cal.solution();
+					  System.out.println("ket qua = "+cal.result());
+					  break;
+			case '/': if (b==0){
+						System.out.println("ko chia dc");
+					  }
+					  else{
+						  cal.solution();
+					 	  System.out.println("ket qua = "+cal.result());
+					 	  break;
+					}
+
+		}
+
 	}
 }
-
