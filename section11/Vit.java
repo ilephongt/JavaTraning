7fddbf12d906cde35794313d8da945eb6a2e485a
lephong <ilephongt@gmail.com>
2015-09-23 16:48:57 +0700

    Create Vit.java

---
+++ section11/Vit.java
@@ -0,0 +1,18 @@
+public class Vit extends GiaCam{
+	public void di(){
+		super.di();
+		System.out.println("Vit co the di");
+	}
+	public void bay(){
+		super.bay();
+		System.out.println("Vit khong the bay");
+	}	
+	public void boi(){
+		super.boi();
+		System.out.println("vit co the boi");
+	}
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la vit");
+	}
+}
