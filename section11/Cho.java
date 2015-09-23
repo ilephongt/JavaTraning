ec3b8d5d92e4a4fba37392e39f6a7b10cf099ba9
lephong <ilephongt@gmail.com>
2015-09-23 16:47:19 +0700

    Create Cho.java

---
+++ section11/Cho.java
@@ -0,0 +1,24 @@
+public class Cho extends GiaSuc{
+	public void di(){
+		super.di();
+		System.out.println("cho co the di");
+	}
+	public void bay(){
+		System.out.println("cho khong bay duoc");
+	}
+	public void boi(){
+		super.boi();
+		System.out.println("cho co the boi");
+	}
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la cho");
+	}
+}
+
+
+
+
+
+
+
