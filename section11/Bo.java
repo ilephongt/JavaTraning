3b35f2438c7da0c552321bdd6aeb9fae5068d899
lephong <ilephongt@gmail.com>
2015-09-23 16:47:54 +0700

    Create Bo.java

---
+++ section11/Bo.java
@@ -0,0 +1,19 @@
+public class Bo extends GiaSuc{
+	public void di(){
+		super.action();
+		System.out.println("Bo co the di");
+	}
+	public void bay(){
+		super.bay();
+		System.out.println("bo khong the bay");
+	}
+	public void boi(){
+		super.boi();
+		System.out.println("bo khong the boi");
+	}
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la Bo");
+	}
+}
+
