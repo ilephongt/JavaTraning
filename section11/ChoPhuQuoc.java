0a84050415829b41c6ec8ccf308ac4b76b25254c
lephong <ilephongt@gmail.com>
2015-09-23 16:56:53 +0700

    Create ChoPhuQuoc.java

---
+++ section11/ChoPhuQuoc.java
@@ -0,0 +1,17 @@
+public class ChoPhuQuoc extends Cho{
+	public void di(){
+		super.di();
+		System.out.println("cho Phu Quoc co the di");
+	}
+	public void bay(){
+		System.out.println("cho Phu Quoc khong bay duoc");
+	}
+	public void boi(){
+		super.boi();
+		System.out.println("cho Phu Quoc co the boi");
+	}
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la Cho Phu Quoc");
+	}
+}
