cf0fb521da5166979be2ab4427c6f5ef1a90dcd4
lephong <ilephongt@gmail.com>
2015-09-23 16:58:11 +0700

    Create GaTa.java

---
+++ section11/GaTa.java
@@ -0,0 +1,21 @@
+
+public class GaTa extends Ga{
+	public void di(){
+		super.di();
+		System.out.println("ga Ta co the di");
+	}
+	public void bay(){
+		super.bay();
+		System.out.println("ga Ta co the bay");
+	}	
+	public void boi(){
+		super.boi();
+		System.out.println("ga Ta khong the boi");
+	}
+	
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la Ga Ta");
+	}
+}
+
