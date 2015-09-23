be8a910652a74d9cac39103f021c7ed07c27b2cb
lephong <ilephongt@gmail.com>
2015-09-23 16:56:09 +0700

    Create ChoNhat.java

---
+++ section11/ChoNhat.java
@@ -0,0 +1,19 @@
+public class ChoNhat extends Cho{
+	public void di(){
+		super.di();
+		System.out.println("cho Nhat co the di");
+	}
+	public void bay(){
+		System.out.println("cho Nhat khong bay duoc");
+	}
+	public void boi(){
+		super.boi();
+		System.out.println("cho Nhat co the boi");
+	}
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la Cho Nhat");
+	}
+}
+
+
