728a91096b31a8094f4f5fe9f7e1861852f91e75
lephong <ilephongt@gmail.com>
2015-09-23 16:45:23 +0700

    Create GiaSuc.java

---
+++ section11/GiaSuc.java
@@ -0,0 +1,16 @@
+public class GiaSuc extends Animal{
+	public void di(){
+		//super.di();
+		//System.out.println("GiaSuc co the di");
+	}
+	public void boi(){
+		//super.boi();
+		//System.out.println("GiaSuc co the boi");
+	}
+ 	public void gioiThieu(){
+ 		//super.gioiThieu();
+ 		//System.out.println("Ta la gia suc");
+ 	}
+}
+
+
