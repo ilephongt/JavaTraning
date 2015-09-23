92de7c19fd0e7086367e13b44165a94a5fd620f6
lephong <ilephongt@gmail.com>
2015-09-23 16:57:47 +0700

    Create GaTay.java

---
+++ section11/GaTay.java
@@ -0,0 +1,18 @@
+public class GaTay extends Ga{
+	public void di(){
+		super.di();
+		System.out.println("ga Tay co the di");
+	}
+	public void bay(){
+		super.bay();
+		System.out.println("ga Tay co the bay");
+	}	
+	public void boi(){
+		super.boi();
+		System.out.println("ga Tay khong the boi");
+	}
+	public void gioiThieu(){
+		super.gioiThieu();
+		System.out.println("ta la Ga Tay");
+	}
+}
