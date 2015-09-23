0169af7e9ecadc3f8122c2219d6a2bf1d392fd8e
lephong <ilephongt@gmail.com>
2015-09-23 16:44:36 +0700

    Create Animal.java

---
+++ section11/Animal.java
@@ -0,0 +1,29 @@
+/*Hãy tự xây dựng class cho các loại động vật với các gợi ý dưới đây
+- động vật
+- gia súc, gia cầm
+- chó, mèo, gà, vịt, chim, bò
+- chó Nhật, chó Phú Quốc
+- Gà Ta, Gà Tây
+- khả năng: đi, bay, bơi
+- nếu loại động vật nào ko có khả năng làm việc gì đó thì in ra “<X> không <Y> được”.  Nếu có khả năng thì in ra “<X> có thể <Y>”
+Ví dụ:  “cho co the di"
+- method gioiThieu: “Ta la <X>"
+*/
+
+class Animal{
+
+	
+	public void di(){
+		//System.out.println("dong vat co the di");
+	}
+	public void bay(){
+		//System.out.println("dong vat co the bay");
+	}
+	public void boi(){
+		//System.out.println("dong vat co the boi");
+	}
+	public void gioiThieu(){
+		//System.out.println("ta la dong vat");
+	}	
+}
+
