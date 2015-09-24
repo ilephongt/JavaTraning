bd95a72f7c3926766b104c13734026163e0d3ea1
lephong <ilephongt@gmail.com>
2015-09-24 17:30:57 +0700

    Create Ga.java

---
+++ setion12/Ga.java
@@ -0,0 +1,13 @@
+ abstract class Ga extends GiaCam{
+	public Ga(String name){
+		super(name);
+
+	}
+	
+	public String di(){
+		return name +" co the di";
+	} 
+	public String boi(){
+		return name +" khong boi duoc";
+	}  
+}
