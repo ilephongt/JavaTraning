a421fd6da393851e5353e10508d8bcafeec69cf4
lephong <ilephongt@gmail.com>
2015-09-24 17:29:04 +0700

    Create Cho.java

---
+++ setion12/Cho.java
@@ -0,0 +1,13 @@
+ abstract class Cho extends GiaSuc{
+	public Cho(String name){
+		super(name);
+
+	}
+	
+	public String bay(){
+		return name +" khong bay duoc";
+	} 
+	public String boi(){
+		return name +" co the boi";
+	}  
+}
