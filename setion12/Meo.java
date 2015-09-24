dea75b0517f17f7382d70654279b22cfd8a0ec3f
lephong <ilephongt@gmail.com>
2015-09-24 17:29:28 +0700

    Create Meo.java

---
+++ setion12/Meo.java
@@ -0,0 +1,11 @@
+class Meo extends GiaSuc{
+	public Meo(String name){
+		super(name);
+	}
+	public String bay(){
+		return name+" khong bay duoc";
+	}
+	public String boi(){
+		return name+" khong boi duoc";
+	}	
+} 
