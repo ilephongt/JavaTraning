2e46d3b0f84e6acf5507f7fc962a3b1fb813df40
lephong <ilephongt@gmail.com>
2015-09-24 12:04:08 +0700

    Update GiaSuc.java

--- section11/GiaSuc.java
+++ section11/GiaSuc.java
@@ -1,15 +1,17 @@
 class GiaSuc extends Animal{
-	public void di(){
-		System.out.println("co the di");
+	private String name = "GIA SUC";
+	
+	public void di(){	
+		System.out.println(name+" co the di");
 	}
 	public void bay(){
-		System.out.println("khong bay duoc");
+		System.out.println(name+" khong bay duoc");
 	}
 	public void boi(){
-		System.out.println("co the boi");
+		System.out.println(name+" co the boi");
 	}
  	public void gioiThieu(){
- 		System.out.println("Ta la gia suc");
+ 		System.out.println("Ta la "+name);
  	}
 }
 
