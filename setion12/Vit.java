ba639c08fa248e9f132ca9d5252085b41c01867b
lephong <ilephongt@gmail.com>
2015-09-24 17:30:08 +0700

    Create Vit.java

---
+++ setion12/Vit.java
@@ -0,0 +1,11 @@
+class Vit extends GiaCam{
+	public Vit(String name){
+		super(name);
+	}
+	public String di(){
+		return name+" co the di";
+	}
+	public String boi(){
+		return name+" co the boi";
+	}	
+} 
