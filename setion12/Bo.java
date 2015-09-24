673df7c6332e78e70aa90d9ec04c171aea6a2a87
lephong <ilephongt@gmail.com>
2015-09-24 17:30:40 +0700

    Create Bo.java

---
+++ setion12/Bo.java
@@ -0,0 +1,11 @@
+class Bo extends GiaSuc{
+	public Bo(String name){
+		super(name);
+	}
+	public String bay(){
+		return name+" khong bay duoc";
+	}
+	public String boi(){
+		return name+" khong boi duoc";
+	}	
+} 
