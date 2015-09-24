41edc610df3da8835888de492a10991e466f7567
lephong <ilephongt@gmail.com>
2015-09-24 12:01:08 +0700

    Update Chim.java

--- section11/Chim.java
+++ section11/Chim.java
@@ -1,16 +1,17 @@
- class Chim extends GiaCam{
-	public void di(){
-		System.out.println("co the di");
+class Chim extends GiaCam{
+	private String name = "CHIM";
+	
+	public void di(){	
+		System.out.println(name+" co the di");
 	}
 	public void bay(){
-		System.out.println("co the bay");
+		System.out.println(name+" co the bay");
 	}
 	public void boi(){
-		System.out.println("co the boi");
+		System.out.println(name+" khong boi duoc");
 	}
+ 	public void gioiThieu(){
+ 		System.out.println("Ta la "+name);
+ 	}
+}
 
-	public void gioiThieu(){
-		System.out.println("ta la Chim");
-	}
-}
-}
