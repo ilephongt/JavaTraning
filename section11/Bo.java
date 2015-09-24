92dc907ef29699ee1a4e139911753d4628432871
lephong <ilephongt@gmail.com>
2015-09-24 12:00:34 +0700

    Update Bo.java

--- section11/Bo.java
+++ section11/Bo.java
@@ -1,15 +1,19 @@
- class Bo extends GiaSuc{
-	public void di(){
-		System.out.println("co the di");
+class Bo extends GiaSuc{
+	private String name = "BO";
+	
+	
+	
+	public void di(){	
+		System.out.println(name+" co the di");
 	}
 	public void bay(){
-		System.out.println("khong the bay");
+		System.out.println(name+" khong bay duoc");
 	}
 	public void boi(){
-		System.out.println("khong the boi");
+		System.out.println(name+" khong boi duoc");
 	}
-	public void gioiThieu(){
-		System.out.println("ta la Bo");
-	}
+ 	public void gioiThieu(){
+ 		System.out.println("Ta la "+name);
+ 	}
 }
 
