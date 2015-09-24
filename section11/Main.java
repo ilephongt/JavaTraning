08206639374125b79267e29ce27ab3fa0ca1bcca
lephong <ilephongt@gmail.com>
2015-09-24 12:04:48 +0700

    Update Main.java

--- section11/Main.java
+++ section11/Main.java
@@ -2,25 +2,38 @@ public class Main{
 
    public static void main(String args[]){
 
-      Animal test = new ChoPhuQuoc();
+      Animal test = new GiaSuc();
       test.gioiThieu();
       test.di();
       test.boi();
       test.bay();
       System.out.println("-------------------------------------------");
-      GiaCam test2 = new Vit();
-	  test2.gioiThieu();
+      GiaSuc test2 = new Bo();
+      test2.gioiThieu();
       test2.di();
       test2.boi();
       test2.bay();
       System.out.println("-------------------------------------------");
-
-      Animal test3 = new GiaSuc();
+      Animal test3 = new GaTa();
       test3.gioiThieu();
       test3.di();
       test3.boi();
       test3.bay();
       System.out.println("-------------------------------------------");
+      GiaCam test4 = new Chim();
+      test4.gioiThieu();
+      test4.di();
+      test4.boi();
+      test4.bay();
+      System.out.println("-------------------------------------------");
+      GiaSuc test5 = new ChoNhat();
+      test5.gioiThieu();
+      test5.di();
+      test5.boi();
+      test5.bay();
+      System.out.println("-------------------------------------------");
+    
+      
 
     }
 }
