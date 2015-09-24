dc7587f89604475706c848df1b433e78c33797c8
lephong <ilephongt@gmail.com>
2015-09-24 12:02:55 +0700

    Update Ga.java

--- section11/Ga.java
+++ section11/Ga.java
@@ -1,14 +1,17 @@
- class Ga extends GiaCam{
-	public void di(){
-		System.out.println("co the di");
+class Ga extends GiaCam{
+	private String name = "GA";
+	
+	public void di(){	
+		System.out.println(name+" co the di");
 	}
 	public void bay(){
-		System.out.println("co the bay");
-	}	
+		System.out.println(name+" co the bay");
+	}
 	public void boi(){
-		System.out.println("khong the boi");
+		System.out.println(name+" khong boi duoc");
 	}
-	public void gioiThieu(){
-		System.out.println("ta la Ga");
-	}
+ 	public void gioiThieu(){
+ 		System.out.println("Ta la "+name);
+ 	}
 }
+
