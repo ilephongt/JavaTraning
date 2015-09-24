aadb41d1a6c86491e3f5425a2818cf0afc3deb4f
lephong <ilephongt@gmail.com>
2015-09-24 17:30:25 +0700

    Create Chim.java

---
+++ setion12/Chim.java
@@ -0,0 +1,11 @@
+class Chim extends GiaCam{
+	public Chim(String name){
+		super(name);
+	}
+	public String di(){
+		return name+" co the di";
+	}
+	public String boi(){
+		return name+" khong boi duoc";
+	}	
+} 
