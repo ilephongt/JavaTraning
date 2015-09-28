85c4fddaf67f63b8053fc377f10a1ce7c1c58e4f
lephong <ilephongt@gmail.com>
2015-09-28 10:07:48 +0700

    Update Work.java

--- section13/Work.java
+++ section13/Work.java
@@ -1,4 +1,3 @@
-
  import java.util.*;
  public class Work{
 
@@ -17,20 +16,62 @@
  				System.out.println("Da bo sung cong viec.");
  				break;
  			case 2:
- 				System.out.println("Day la danh sach cong viec:");
- 				int k=0;
- 				for(int i = 0; i < list.size();	i++){
- 					k++;
- 					System.out.println(k+". "+list.get(i));
+ 				if(list.isEmpty()){
+ 					System.out.println("Hien tai chua co cong viec nao!");
+ 					break;
  				}
- 				break;
+ 				else{
+ 					System.out.println("Day la danh sach cong viec:");
+ 					int k=0;
+ 					for(int i = 0; i < list.size();	i++){
+ 						k++;
+ 						System.out.println(k+". "+list.get(i));
+ 					}
+ 					break;
+ 				}
+ 			
  			case 3:
- 				System.out.println("So hieu cong viec muon xoa?");
- 				int j = Integer.parseInt(sc.nextLine());
- 				j--;
- 				list.remove(j);
- 				 System.out.println("Da xoa cong viec.");
- 				break;
+ 					boolean f = false;
+ 					int k;
+ 				if(!list.isEmpty()){
+
+ 					do{	
+ 						try{
+ 							System.out.println("So hieu cong viec muon xoa?");
+            				k=Integer.parseInt(sc.nextLine());
+            					if(k>0 && k<list.size()){
+ 									k--;
+ 									list.remove(k);
+ 									System.out.println("Da xoa cong viec.");
+ 									f = true;
+ 									break;
+        						}
+        						else{
+ 									System.out.println("Nhap sai vi tri xoa, ve Menu chinh!");
+ 									break;
+ 								}
+
+        				}
+
+       					catch(NumberFormatException ex){
+            				System.out.println("Chi nhap so nguyen, ve menu chinh!");
+            				f = false;
+       					}
+        				f = true;
+ 						
+ 						 				
+					
+ 						
+ 					}
+ 					while(f == false);
+ 				}
+
+ 				else {
+ 					System.out.println("Hien tai ko co cong viec nao de xoa!");
+ 					break;
+ 				}
+ 				 break;
+
  			case 4:
  				System.out.println("Thoat chuong trinh...");
  				break;
@@ -65,7 +106,7 @@
 					case "4": w.solution(4);
 						flag = true;
 						break;
-					default: System.out.println("ban nhap gia tri khong dung!");
+					default: System.out.println("Chi nhap tu 1-4!");
 				}	
 			}
 		while (flag == false);
